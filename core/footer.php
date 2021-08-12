1eb6d1e627d41a17129b85c102697b61583fe3aa
Alexis Bonnin <kanzaki.urumy@gmail.com>
2021-08-12 23:20:52 +0200

    side bar and content

--- core/footer.php
+++ core/footer.php
@@ -1,6 +1,9 @@
-</main>
-    <footer>
-
-    </footer>
+                </main>
+                <footer class="row bg-primary">
+                    <div class="col text-center">Kanzaki Dev</div>
+                </footer>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
--- core/header.php
+++ core/header.php
@@ -7,7 +7,69 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 </head>
 <body>
-    <header>
-        
-    </header>
-    <main>
+    <div class="container-fluid overflow-hidden">
+        <div class="row vh-100 overflow-auto">
+            <header class="col-12 col-sm-3 col-xl-2 px-sm-2 px-0 bg-dark d-flex sticky-top pb-2 pb-sm-0">
+                <div class="d-flex flex-sm-column flex-row flex-grow-1 align-items-center align-items-sm-start px-1 pt-2 text-white">
+                    <span  class="d-flex align-items-center pb-sm-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                        <span class="fs-4">Manga</span>
+                    </span>
+                    <ul class="nav nav-pills flex-sm-column flex-row flex-nowrap flex-shrink-1 flex-sm-grow-0 flex-grow-1 mb-sm-auto mb-0 justify-content-center align-items-center align-items-sm-start" id="menu">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link px-sm-0 px-2">
+                                <i class="fs-5 bi-house"></i><span class="ms-1 d-none d-sm-inline">Possédés</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link px-sm-0 px-2">
+                                <i class="fs-5 bi-house"></i><span class="ms-1 d-none d-sm-inline">Souhaités</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link px-sm-0 px-2">
+                                <i class="fs-5 bi-house"></i><span class="ms-1 d-none d-sm-inline">Manquants</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link px-sm-0 px-2">
+                                <i class="fs-5 bi-house"></i><span class="ms-1 d-none d-sm-inline">Ajout Collection</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="#" class="nav-link px-sm-0 px-2">
+                                <i class="fs-5 bi-house"></i><span class="ms-1 d-none d-sm-inline">Ajout Tome</span>
+                            </a>
+                        </li>
+                    </ul>
+                    <div class="dropdown py-sm-4 mt-sm-auto ms-auto ms-sm-0 flex-shrink-1">
+                        <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="https://avatars.githubusercontent.com/u/49068145?s=400&u=afa4e28bd124af2977d352c2992297f5b21792d1&v=4" alt="hugenerd" width="28" height="28" class="rounded-circle">
+                            <span class="d-none d-sm-inline mx-1">Kanzaki Urumi</span>
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-dark text-small shadow" aria-labelledby="dropdownUser1">
+                            <li><a class="dropdown-item" href="#">New project...</a></li>
+                            <li><a class="dropdown-item" href="#">Settings</a></li>
+                            <li><a class="dropdown-item" href="#">Profile</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Sign out</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </header>
+            <div class="col d-flex flex-column h-100">
+                <main class="row overflow-auto pt-2">
+                    <div class="col-12 collection-gallery mx-auto">
+                        <h3>Goblin Slayer</h3>
+                        <img class="mx-2" src="files/img/minicover/1.jpg" />
+                        <img class="mx-2" src="files/img/minicover/2.jpg" />
+                        <img class="mx-2" src="files/img/minicover/3.jpg" />
+                        <img class="mx-2" src="files/img/minicover/4.jpg" />
+                        <img class="mx-2" src="files/img/minicover/5.jpg" />
+                        <img class="mx-2" src="files/img/minicover/6.jpg" />
+                        <img class="mx-2" src="files/img/minicover/7.jpg" />
+                        <img class="mx-2" src="files/img/minicover/8.jpg" />
+                        <img class="mx-2" src="files/img/minicover/9.jpg" />
+                        <img class="mx-2" src="files/img/minicover/10.jpg" />
+                    </div>
