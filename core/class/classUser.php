bc11447ac368b2f166cf37c5e55c895f369be051
Alexis Bonnin <kanzaki.urumy@gmail.com>
2021-08-18 23:07:20 +0200

    user class

--- core/class/classUser.php
+++ core/class/classUser.php
@@ -26,16 +26,16 @@ class User{
     public function readLibrary():User|false
     {
         // Left join, case where user havent any tome for Manga name
-        $sql = "SELECT `um`.`id_manga`, `um`.`id_tome`, `um`.`id_tome_status` 
+        $sql = "SELECT `um`.`id_manga`, `ut`.`id_tome`, `ut`.`id_tome_status` 
         FROM `user_manga` AS `um` 
         LEFT JOIN `user_tome` AS `ut` ON `um`.`id_user_manga` = `ut`.`id_user_manga` 
-        INNER JOIN `manga` AS `m` ON `id_manga` = `um`.`id_manga` 
-        INNER JOIN `manga_tome` AS `mt` ON `mt`.`id_tome` = `um`.`id_tome`
-        WHERE `um`.`id_user_tome` = ".intval($this->idMembre)." 
+        INNER JOIN `manga` AS `m` ON `m`.`id_manga` = `um`.`id_manga` 
+        INNER JOIN `manga_tome` AS `mt` ON `mt`.`id_tome` = `ut`.`id_tome`
+        WHERE `um`.`id_user` = ".intval($this->idMembre)." 
         ORDER BY `m`.`name` ASC, `mt`.`number` ASC";
 
         foreach($this->PDO->query($sql)->fetchObj() as $elem){
-            $this->manga[$elem->id_manga] = [$elem->id_tome => $id_tome_status];
+            $this->manga[$elem->id_manga][$elem->id_tome] = $elem->id_tome_status;
         }
 
         return $this;
