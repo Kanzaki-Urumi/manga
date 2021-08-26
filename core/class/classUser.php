a5340d444f799544f1be539dc1b931df1b840c63
Alexis Bonnin <kanzaki.urumy@gmail.com>
2021-08-26 23:24:11 +0200

    update User request

--- core/class/classUser.php
+++ core/class/classUser.php
@@ -1,6 +1,7 @@
 <?php
 class User{
 
+    public string $test = '';
     public string $pseudo = '';
     public int|null $idType = null;
     public array $manga = [];
@@ -19,7 +20,7 @@ class User{
 
         if($idMembre > 0){
             return $this
-                ->readLibrary()
+                ->getLibraryByState(1,[1],null)
                 ->getMangaHave()
                 ->getMangaWish()
                 ->getMangaHavent();
@@ -47,6 +48,56 @@ class User{
         }
         return $this;
     }
+
+    public function getLibraryByState(null|array $idGenre = null, null|array $idCategorie = null, null|int $idTomeState = null):User|false
+    {
+        $sql = "SELECT `um`.`id_manga`, `ut`.`id_tome`, `ut`.`id_tome_status` 
+        FROM `user_manga` AS `um` ";
+
+        if($idTomeState > 0){
+           $sql .= "INNER JOIN `user_tome` AS `ut` ON `um`.`id_user_manga` = `ut`.`id_user_manga` AND  `ut`.`id_tome_status` = ".$idTomeState." ";
+        }else{
+            $sql .= "LEFT JOIN `user_tome` AS `ut` ON `um`.`id_user_manga` = `ut`.`id_user_manga` ";
+        }
+
+        $sql .= "INNER JOIN `manga` AS `m` ON `m`.`id_manga` = `um`.`id_manga` ";
+        if($idGenre != null){
+            $sql .= "AND `m`.`id_genre` IN(".implode(',',$idGenre).") ";
+        }
+
+        if($idCategorie != null){
+            $sql .= "LEFT JOIN `manga_categorie` AS `mc` ON `mc`.`id_manga` = `m`.`id_manga` AND `mc`.`id_categorie` IN(".implode(',',$idCategorie).") ";
+        }
+
+        
+        $sql .= "INNER JOIN `manga_tome` AS `mt` ON `mt`.`id_tome` = `ut`.`id_tome`
+        WHERE `um`.`id_user` = ".intval($this->idMembre)." 
+        ORDER BY `m`.`name` ASC, `mt`.`number` ASC";
+
+        foreach($this->PDO->query($sql)->fetchObj() as $elem){
+            $this->manga[(int) $elem->id_manga][(int) $elem->id_tome] = (int) $elem->id_tome_status;
+        }
+        $this->test = $sql;
+        return $this;
+    }
+
+    public function getLibraryFiltered(int|false $idGenre = false, int|false $idCategorie = false, int|false $idPossession = false)
+    {
+        // Left join, case where user havent any tome for Manga name
+        $sql = "SELECT `um`.`id_manga`, `ut`.`id_tome`, `ut`.`id_tome_status` 
+        FROM `user_manga` AS `um` 
+        LEFT JOIN `user_tome` AS `ut` ON `um`.`id_user_manga` = `ut`.`id_user_manga` 
+        INNER JOIN `manga` AS `m` ON `m`.`id_manga` = `um`.`id_manga` 
+        INNER JOIN `manga_tome` AS `mt` ON `mt`.`id_tome` = `ut`.`id_tome`
+        WHERE `um`.`id_user` = ".intval($this->idMembre)." AND `m`.`id_genre` = ".$id." 
+        ORDER BY `m`.`name` ASC, `mt`.`number` ASC";
+
+        foreach($this->PDO->query($sql)->fetchObj() as $elem){
+            $this->manga[(int) $elem->id_manga][(int) $elem->id_tome] = (int) $elem->id_tome_status;
+        }
+        return $this;
+    }
+
 
     /**
      * getListGenre
